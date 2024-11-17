99d8bf8465bfc47d92d7ef0ab99ec5786c96af79
ahzamir <ahmadzamiryousofi1397@gmail.com>
2024-11-17 03:47:41 +0100

    added some chages

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Connect to the database, then start the server
 connectDB().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log("Connected to database");
 }).catch(error => {
     console.error("Failed to connect to database:", error);
 });
@@ -23,4 +23,4 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
-export default app;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
