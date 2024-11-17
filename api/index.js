5d8ecdd696d2825a373c77be840b0f9b8d5d7a12
ahzamir <ahmadzamiryousofi1397@gmail.com>
2024-11-17 03:36:07 +0100

    added some changes to the project

--- api/index.js
+++
@@ -1,6 +0,0 @@
-import app from "../app";
-
-// serverless is a utility that allows you to run Express.js apps on vercel
-export default async function (req, res) {
-    await app(req, res);
-}
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// server.js
-import app from "./app.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 import connectDB from "./config/db.js"; // Import the database configuration
 
+dotenv.config();
+
+const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 // Connect to the database, then start the server
@@ -10,5 +17,10 @@ connectDB().then(() => {
 }).catch(error => {
     console.error("Failed to connect to database:", error);
 });
+
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 export default app;
